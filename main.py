a8e600a71d4fd09a68d75d7620ebc2c288eb9fa5
Ethan <bigeskelly@gmail.com>
2022-12-08 16:58:19 -0700

    merge

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main_Ian(i):
         start_hyp_dict = {'p_w': [0.1, 0.3, 0.5, 0.7, 0.9], 
                           'p_c': [1.3, 1.5, 1.7, 1.9, 2.1],
                           'pop_size': [10, 20, 30, 40, 50]}
-        CV.analysis("PSO",0,['p_w', 'p_c', 'pop_size'], start_hyp_dict)
+        CV.analysis("PSO",2,['p_w', 'p_c', 'pop_size'], start_hyp_dict)
     if i == 5:
         DD = DataDictionary()
         data = DD.dataobject(True, "Hardware")
@@ -45,7 +45,7 @@ def main_Ian(i):
         start_hyp_dict = {'p_c': [0.75, 0.8, 0.85, 0.9, 0.95],
                           'p_m': [0.01, 0.02, 0.03, 0.04, 0.05],
                           'pop_size': [10, 20, 30, 40, 50]}
-        CV.analysis("GA",1,['p_c', 'p_m', 'pop_size'], start_hyp_dict)
+        CV.analysis("DE",0,['p_c', 'p_m', 'pop_size'], start_hyp_dict)
 
 
 
@@ -60,7 +60,7 @@ def mainEthan():
     # print(P.predict_with_chr(chr, 2))
     
 if __name__=="__main__":
-    main_Ian(5)
+    main_Ian(4)
     #mainEthan()
 
 
