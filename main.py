0dd9ca4c3427970fcfceb5e8b482f13e6def25f8
Ethan10Real <ethan@10Real.com>
2022-12-09 12:30:10 -0700

    merging

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main_Ian(i):
         print(P.predict_with_chr(chr, 2))
     if i == 4:
         DD = DataDictionary()
-        data = DD.dataobject(True, "Hardware")
+        data = DD.dataobject(True, "BreastCancer")
         CV = CrossValidation(data)
         start_hyp_dict = {'p_w': [0.1, 0.3, 0.5, 0.7, 0.9], 
                           'p_c': [1.3, 1.5, 1.7, 1.9, 2.1],
@@ -40,12 +40,12 @@ def main_Ian(i):
         CV.analysis("PSO",2,['p_w', 'p_c', 'pop_size'], start_hyp_dict)
     if i == 5:
         DD = DataDictionary()
-        data = DD.dataobject(True, "Hardware")
+        data = DD.dataobject(True, "Abalone")
         CV = CrossValidation(data)
         start_hyp_dict = {'p_c': [0.75, 0.8, 0.85, 0.9, 0.95],
                           'p_m': [0.01, 0.02, 0.03, 0.04, 0.05],
                           'pop_size': [10, 20, 30, 40, 50]}
-        CV.analysis("DE",0,['p_c', 'p_m', 'pop_size'], start_hyp_dict)
+        CV.analysis("DE",2,['p_c', 'p_m', 'pop_size'], start_hyp_dict)
 
 
 
@@ -60,8 +60,8 @@ def mainEthan():
     # print(P.predict_with_chr(chr, 2))
     
 if __name__=="__main__":
-    main_Ian(2)
-    #main_Ian(5)
+    #main_Ian(2)
+    main_Ian(4)
     #mainEthan()
 
 
